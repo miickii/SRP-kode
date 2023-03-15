095a6ddd34c938a49ba058a81a7117ee8253df02
miickii <mickiharning@gmail.com>
2023-03-15 06:15:10 +0100

    Update mnist_model.py

--- Digits/mnist_model.py
+++ Digits/mnist_model.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-# Load the MNIST dataset
 mnist = tf.keras.datasets.mnist
 
 def plot_images(x, y):
@@ -16,7 +15,7 @@ def plot_images(x, y):
     
     plt.show()
 
-# Split the dataset into training and test datasets
+# Split the datasæt i træningsdata og testdata
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = np.loadtxt('x_augmented.txt', dtype=int)
@@ -28,10 +27,9 @@ x_train = x_train.reshape((1140000, 28, 28))
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Generate a random permutation of the indices
 perm = np.random.permutation(x_train.shape[0])
 
-# Shuffle both arrays in the same way
+# Bland rækkefølgen af træningseksempler
 x_train = x_train[perm]
 y_train = y_train[perm]
 
@@ -42,16 +40,13 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-# Compile the model
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 
-# Train the model
 model.fit(x_train, y_train, epochs=5)
 
-# Evaluate the model
 model.evaluate(x_test, y_test, verbose=2)
 
 model.save('digit_model.h5')
