70abd65db2a7fd73b36f0d26d54d71f4e555b492
miickii <mickiharning@gmail.com>
2023-03-15 06:14:02 +0100

    Update augment_mnist.py

--- Digits/augment_mnist.py
+++ Digits/augment_mnist.py
@@ -32,21 +32,7 @@ def augment_images(x, y):
 
     return np.array(x_augmented), np.array(y_augmented)
 
-def show_transformed(original, transformed):
-  fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(8, 10))
-  ax[0, 0].imshow(original, cmap="gray")
-  ax[0, 0].set_title('Original')
-
-  # Display the transformed images in a 2x2 grid
-  ax[1, 0].imshow(transformed[0], cmap="gray")
-  ax[1, 1].imshow(transformed[1], cmap="gray")
-  ax[2, 0].imshow(transformed[2], cmap="gray")
-  ax[2, 1].imshow(transformed[3], cmap="gray")
-
-  fig.tight_layout()
-  plt.show()
-
-# Split the dataset into training and test datasets
+# Split the datasæt i træningsdata og testdata
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_augmented, y_augmented = augment_images(x_train, y_train)
